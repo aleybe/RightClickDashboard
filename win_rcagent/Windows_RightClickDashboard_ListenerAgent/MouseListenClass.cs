b07eb9b81dc6b9af49dcb13e6dcc0b14e3f7c989
Alex <34798834+aleybe@users.noreply.github.com>
2018-11-13 23:47:53 +0800

    C# Mouse Listening Agent
    This currently just detects mouse listening.

---
+++ win_rcagent/Windows_RightClickDashboard_ListenerAgent/MouseListenClass.cs
@@ -0,0 +1,24 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_RightClickDashboard_ListenerAgent
+{
+    class MouseListenClass
+    {
+
+        public static void ListenForMouseClicks()
+        {
+            Console.WriteLine("listening...");
+
+            Hook.GlobalEvents().MouseDown += async (sender, e) =>
+            {
+                Console.WriteLine($"Mouse {e.Button} down");
+            };
+        }
+
+    }
+}
---
+++ win_rcagent/Windows_RightClickDashboard_ListenerAgent/Program.cs
@@ -0,0 +1,24 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_RightClickDashboard_ListenerAgent
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            MouseListenClass.ListenForMouseClicks();
+
+            Application.Run(new ApplicationContext());
+            
+        }
+
+
+    }
+}
