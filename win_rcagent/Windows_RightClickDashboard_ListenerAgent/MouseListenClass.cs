2022033bd6fb47479bd8390156ff4ced97472df0
Alex <34798834+aleybe@users.noreply.github.com>
2018-11-25 20:56:57 +0800

    Lambda Function and DynamoDB
    Added Lambda Function Files

--- win_rcagent/Windows_RightClickDashboard_ListenerAgent/MouseListenClass.cs
+++ win_rcagent/Windows_RightClickDashboard_ListenerAgent/MouseListenClass.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,30 @@ namespace Windows_RightClickDashboard_ListenerAgent
             {
                 Console.WriteLine($"Mouse {e.Button} down");
             };
+        }
+
+        public async Task<string> SendMouseClicks(DateTime date, string button)
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Method = "POST";
+            request.PreAuthenticate = true;
+            
+            using (Stream requestBody = request.GetRequestStream())
+            {
+                await requestBody.WriteAsync(dataBytes, 0, dataBytes.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
         }
 
     }
